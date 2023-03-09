da3de71c31faf1af4d6b2bf6f8b17eb521fc28fe
TimMayr <tim.mayr@gmx.at>
2023-03-09 13:16:04 +0100

    presentation

---
+++ reveal.js-master/index.js
@@ -0,0 +1,7 @@
+// More info about initialization & config:
+// - https://revealjs.com/initialization/
+// - https://revealjs.com/config/
+Reveal.initialize({
+	hash   : true,
+	plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+});
